cfc5c6a318526d7aca52cf5f11b4937d6a7868d1
Bongers <jan@ubeeo.nl>
2024-12-05 21:28:42 +0100

    2015 Day 01 complete

---
+++ src/main/java/tech/bongers/aoc/aoc2015/Year2015.java
@@ -0,0 +1,12 @@
+package tech.bongers.aoc.aoc2015;
+
+import tech.bongers.aoc.AdventOfCode;
+
+import java.util.List;
+
+public abstract class Year2015 extends AdventOfCode {
+
+    public List<String> getPuzzleInputForDay(final String day) {
+        return getPuzzleInputForYearAndDay("2015", day);
+    }
+}
---
+++ src/main/java/tech/bongers/aoc/aoc2015/day/Day01.java
@@ -0,0 +1,43 @@
+package tech.bongers.aoc.aoc2015.day;
+
+import tech.bongers.aoc.aoc2015.Year2015;
+
+import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Day01 extends Year2015 {
+
+    public static void main(final String[] args) {
+        new Day01().doPuzzle();
+    }
+
+    @Override
+    public void doPuzzle() {
+        final List<String> instructions = getPuzzleInputForDay("01");
+        calculateFloorDestination(instructions);
+    }
+
+    private void calculateFloorDestination(final List<String> instructions) {
+        final AtomicInteger floor = new AtomicInteger();
+        final AtomicBoolean basementReached = new AtomicBoolean(false);
+
+        instructions.forEach(instruction -> {
+            final char[] chars = instruction.toCharArray();
+            for (int i = 0; i < chars.length; i++) {
+                if (chars[i] == '(') {
+                    floor.getAndIncrement();
+                } else {
+                    floor.getAndDecrement();
+                }
+
+                if (floor.get() == -1 && !basementReached.get()) {
+                    basementReached.set(true);
+                    log("Char position first basement entry: {}", i + 1);
+                }
+            }
+        });
+
+        log("Floor destination: {}", floor);
+    }
+}
